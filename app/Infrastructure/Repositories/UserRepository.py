ab0bb681cffe2bfc3939e11d1743a31c6007b97b
cubizkeyo0o <nguyenlongnamno1@gmail.com>
2025-02-18 02:51:53 +0700

    convert sync to async

--- app/Infrastructure/Repositories/UserRepository.py
+++ app/Infrastructure/Repositories/UserRepository.py
@@ -1,33 +1,74 @@
-from typing import List, Optional
-from asyncio import wait
-
 from fastapi import Depends
-from sqlalchemy.orm import Session, lazyload
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 from app.Infrastructure.database.DatabaseInit import (
     get_db_connection,
 )
-from app.Domain.Entities.User import CreateUserRequest, User
-from app.Infrastructure.database.mapping import User as userRepository
-from app.API.Models.UserModel import ResponseUserModel
+from app.Domain.Entities.User import (
+    User as DbUser,
+    UserCreate, 
+    UserUpdate
+)
 
 class UserRepository:
-    db: Session
+    db: AsyncSession
 
-    def __init__(
-        self, db: Session = Depends(get_db_connection)
-    ) -> None:
+    def __init__(self, db: AsyncSession = Depends(get_db_connection)) -> None:
         self.db = db
-    
-    def create(self, user_request: CreateUserRequest) -> ResponseUserModel:
-        user_create = userRepository(user_request.name, user_request.email)
-        self.db.add(user_create)
-        self.db.commit()
-        self.db.refresh(user_create)
-        return user_create
-    
-    def update(self, id: int, user: User) -> ResponseUserModel:
-        user.id = id
-        self.db.merge(user)
-        self.db.commit()
+
+    async def get_by_email_async(self, email: str ):
+        query = select(DbUser).filter_by(email=email)
+        queryResult = await self.db.execute(query)
+        user = queryResult.scalars().first()
+        if not user:
+            return None
         return user
+        
+    async def get_by_id_async(self, id: int):
+        query = select(DbUser).filter_by(id=id)
+        queryResult = await self.db.execute(query)
+        user = queryResult.scalars().first()
+        if not user:
+            return None
+        return user
+
+    async def create_async(self, user: UserCreate):
+        async with get_db_connection() as db:
+            user_create = DbUser(**user.model_dump())
+            try:
+                db.add(user_create)
+                await db.commit()
+                await db.refresh(user_create)
+            except Exception as ex:
+                raise Exception("abc") from ex
+
+            return user_create
+    
+    async def update_async(self, id: int, user: UserUpdate):
+        query = select(DbUser).filter_by(id=id)
+        queryResult = await self.db.execute(query)
+        existing_user = queryResult.scalars().first()
+
+        if not existing_user:
+            return None
+        
+        for field, value in user.model_dump(exclude_unset=True).items:
+            setattr(existing_user, field, value)
+        
+        await self.db.commit()
+        return existing_user
+    
+    async def delete_async(self, id: int):
+        query = select(DbUser).filter_by(id=id)
+        queryResult = await self.db.execute(query)
+        existing_user = queryResult.scalars().first()
+
+        if not existing_user:
+            return None
+        
+        await self.db.delete(existing_user)
+        return {"message": "User deleted successfully"}
+
+async def get_user_repository(db: AsyncSession = Depends(get_db_connection)) -> UserRepository:
+        return UserRepository(db)
--- app/Infrastructure/database/DatabaseInit.py
+++ app/Infrastructure/database/DatabaseInit.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from app.Domain.Base import EntityMeta
 
 #from app.Infrastructure.Config.Enviroment import get_enviroment_variables
 
@@ -7,21 +8,24 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 #env = get_enviroment_variables()
 
 # Generate Database URL
-DATABASE_URL = f"{"mysql+mysqlconnector"}://{"root"}:{"longNam2403"}@{"localhost"}:{"3306"}/{"testdatabase"}"
+DATABASE_URL = f"{"mysql+asyncmy"}://{"root"}:{"longNam2403"}@{"localhost"}:{"3306"}/{"testdatabase"}"
 
 # Create Database Engine
-Engine = create_engine(
+Engine = create_async_engine(
     DATABASE_URL, echo=True, future=True
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=Engine
-)
+async def init_models():
+    async with Engine.begin() as conn:
+        # await conn.run_sync(EntityMeta.metadata.drop_all)  # Uncomment nếu muốn xóa các bảng cũ
+        await conn.run_sync(EntityMeta.metadata.create_all)
 
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=Engine)
 
-def get_db_connection():
-    db = scoped_session(SessionLocal)
+@asynccontextmanager
+async def get_db_connection():
+    db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()
+        await db.close()
