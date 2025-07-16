7d31f3c6feba95ffb74eb7b1064a66d002cd90c8
cubizkeyo0o <nguyenlongnamno1@gmail.com>
2025-07-16 23:18:49 +0700

    update table blacklist_tokens

--- app/domain/models/black_list_token.py
+++ app/domain/models/black_list_token.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.infrastructure.database.database_init import Base
 
 class BlackListToken(Base):
-    __tablename__ = "blacklisttokens"
+    __tablename__ = "blacklist_tokens"
 
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4 ,index=True)
-    expire: Mapped[datetime]
+    expire: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc), nullable=False)
     created_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc), nullable=False)
